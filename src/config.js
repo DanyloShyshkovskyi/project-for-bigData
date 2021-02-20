1cb4b43ef02da21bb59c42558204bb6f3c5c24bc
DanyloShyshkovskyi <danik808@gmail.com>
2021-02-20 18:58:27 +0200

    first commit

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {}
 
-config.host = process.env.HOST || 'https://localhost:8081/'
+config.host = process.env.HOST || 'https://bigdataproject.documents.azure.com:443/'
 config.authKey =
   process.env.AUTH_KEY ||
-  'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
+  'VGbbVU5pWZzndByzyrUiN1YcUac5NQgRJsfzfX7T3x0PEmokouiIBER6AfEecCcIevVhovjfxPOBZ51ophb5sA=='
 config.databaseId = 'ToDoList'
 config.containerId = 'Items'
 
